958c96421b0b26148c953f209c2273f531678af9
smnschmnck <simon.schmeinck@gmx.de>
2023-05-05 19:23:57 +0200

    Custom sign in

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="w-full flex justify-center">
+      <SignUp />
+    </div>
+  );
 }
